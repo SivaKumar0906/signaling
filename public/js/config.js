2e0cdb5e7e3fe4b3e8af165b4701fda06be6eaeb
SivaKumar0906 <87262057+SivaKumar0906@users.noreply.github.com>
2022-09-19 04:55:58 +0530

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,3 @@
+turnConfig = {
+    iceServers: [{   urls: [ "stun:bn-turn1.xirsys.com" ]}, {   username: "uyTsJVdAVlOiWRcgaYuEOWh6iGiz4uTOi7fctIuBFP4ftCMR6VLvkxF04vGInDDmAAAAAGMgiC93ZWJkZXYwOTA2",   credential: "8fe78342-3369-11ed-949d-0242ac140004",   urls: [       "turn:bn-turn1.xirsys.com:80?transport=udp",       "turn:bn-turn1.xirsys.com:3478?transport=udp",       "turn:bn-turn1.xirsys.com:80?transport=tcp",       "turn:bn-turn1.xirsys.com:3478?transport=tcp",       "turns:bn-turn1.xirsys.com:443?transport=tcp",       "turns:bn-turn1.xirsys.com:5349?transport=tcp"   ]}]
+}
